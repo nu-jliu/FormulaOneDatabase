498574efbd77389bcc18387ed687e174ec0d2076
NiehYunko <nieh_yunk@163.com>
2020-02-04 01:23:16 -0500

    add deleteQuery in Likes window

--- Database Connection/src/UI/LikesWindow.java
+++ Database Connection/src/UI/LikesWindow.java
@@ -22,33 +22,33 @@ public class LikesWindow {
 	private JFrame frame;
 	private Connections dbService;
 	private int UID;
-	
+
 	public LikesWindow(Connections dbservice, int UID) {
 		this.dbService = dbservice;
 		this.UID = UID;
 		this.frame = new JFrame("Formula1Tracker");
 		this.frame.setBounds(100, 150, 750, 543);
-		
+
 		JPanel newPanel = new JPanel();
-		
+
 		DriverService driverService = new DriverService(this.dbService);
 		ArrayList<String> driverNames = driverService.getDriverNames();
 		JComboBox<String> drivers = new JComboBox<>();
-		for (String s : driverNames) 
+		for (String s : driverNames)
 			drivers.addItem(s);
 		newPanel.add(drivers, BorderLayout.WEST);
-		
+
 		TeamService teamService = new TeamService(this.dbService);
 		ArrayList<String> teamNames = teamService.getTeamNameList();
 		JComboBox<String> teams = new JComboBox<>();
 		for (String s : teamNames)
 			teams.addItem(s);
 		newPanel.add(teams, BorderLayout.EAST);
-		
+
 		this.frame.getContentPane().add(newPanel, BorderLayout.CENTER);
-		
+
 		JPanel buttonPanel = new JPanel();
-		
+
 		JButton likeDriver = new JButton("Like Driver");
 		likeDriver.addActionListener(new ActionListener() {
 
@@ -57,10 +57,10 @@ public class LikesWindow {
 				String name = (String) drivers.getSelectedItem();
 				LikesWindow.this.likeQuery("Drive", name);
 			}
-			
+
 		});
 		buttonPanel.add(likeDriver, BorderLayout.WEST);
-		
+
 		JButton likeTeam = new JButton("Like Team");
 		likeTeam.addActionListener(new ActionListener() {
 
@@ -69,12 +69,39 @@ public class LikesWindow {
 				String name = (String) teams.getSelectedItem();
 				LikesWindow.this.likeQuery("Team", name);
 			}
-			
+
 		});
 		buttonPanel.add(likeTeam, BorderLayout.EAST);
-		
+
 		this.frame.add(buttonPanel, BorderLayout.NORTH);
+
+		JButton deleteLikedDriver = new JButton("Delete Driver");
+		deleteLikedDriver.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String name = (String) teams.getSelectedItem();
+				LikesWindow.this.deleteQuery("Driver", name);
+			}
+
+		}); 
+		deleteLikedDriver.setBounds(200, 300, 100, 25);
 		
+		JButton deleteLikedTeam = new JButton("Delete Team");
+		deleteLikedTeam.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String name = (String) teams.getSelectedItem();
+				LikesWindow.this.deleteQuery("Team", name);
+			}
+
+		}); 
+		
+		deleteLikedTeam.setBounds(200, 350, 100, 25);
+		this.frame.getContentPane().add(deleteLikedTeam, BorderLayout.SOUTH);
+		this.frame.getContentPane().add(deleteLikedDriver, BorderLayout.SOUTH);
+
 		JButton back = new JButton("Return");
 		back.setBounds(200, 500, 100, 25);
 		back.addActionListener(new ActionListener() {
@@ -84,18 +111,18 @@ public class LikesWindow {
 				LikesWindow.this.closeFrame();
 				new UpdateWindow(LikesWindow.this.dbService, LikesWindow.this.UID);
 			}
-			
+
 		});
 		this.frame.getContentPane().add(back, BorderLayout.SOUTH);
-		
+
 		this.frame.setVisible(true);
 		this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
-	
+
 	public void closeFrame() {
 		this.frame.dispose();
 	}
-	
+
 	public void likeQuery(String item, String name) {
 		try {
 			CallableStatement cs1 = this.dbService.getConnection().prepareCall("{? = call get_" + item + "ID(?)}");
@@ -116,8 +143,7 @@ public class LikesWindow {
 			if (errorCode == 1) {
 				JOptionPane.showMessageDialog(null, "User or liked item not exist");
 				return;
-			}
-			else if (errorCode == 2) {
+			} else if (errorCode == 2) {
 				JOptionPane.showMessageDialog(null, "The item is already in your favorite list");
 				return;
 			}
@@ -127,6 +153,38 @@ public class LikesWindow {
 			e.printStackTrace();
 			return;
 		}
-		
+
+	}
+
+	public void deleteQuery(String item, String name) {
+		try {
+			CallableStatement cs1 = this.dbService.getConnection().prepareCall("{? = call get_" + item + "ID(?)}");
+			cs1.setString(2, name);
+			cs1.registerOutParameter(1, Types.INTEGER);
+			cs1.execute();
+			int id = cs1.getInt(1);
+			if (id == -1) {
+				JOptionPane.showMessageDialog(null, "Invalid Name Input");
+				return;
+			}
+			CallableStatement cs2 = this.dbService.getConnection().prepareCall("{? = call delete_Star_" + item + "(?, ?)}");
+			cs2.setInt(2, this.UID);
+			cs2.setInt(3, id);
+			cs2.registerOutParameter(1, Types.INTEGER);
+			cs2.execute();
+			int errorCode = cs2.getInt(1);
+			if (errorCode == 1) {
+				JOptionPane.showMessageDialog(null, "User or liked item not exist");
+				return;
+			} else if (errorCode == 2) {
+				JOptionPane.showMessageDialog(null, "The item is not in your favorite list");
+				return;
+			}
+			return;
+		} catch (SQLException e) {
+			JOptionPane.showMessageDialog(null, "Failed to delete");
+			e.printStackTrace();
+			return;
+		}
 	}
 }
