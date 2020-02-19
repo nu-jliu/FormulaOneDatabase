accc1c0b0b337d5049ff672f8fb4a6dfd4771959
NiehYunko <nieh_yunk@163.com>
2020-02-18 20:27:38 -0500

    add year box in Navigation window

--- Database Connection/src/UI/NavigationWindow.java
+++ Database Connection/src/UI/NavigationWindow.java
@@ -6,8 +6,12 @@ import java.awt.event.ActionListener;
 import java.sql.CallableStatement;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
 import java.sql.Types;
+import java.util.ArrayList;
+
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
@@ -18,6 +22,7 @@ import javax.swing.table.TableCellRenderer;
 import javax.swing.table.TableColumn;
 
 import Database.service.Connections;
+import Database.service.DriverService;
 import Database.service.WatchService;
 
 
@@ -48,6 +53,15 @@ public class NavigationWindow {
 		this.scrollpane.setBounds(43, 33, 597, 200);
 		this.Table.setModel(model);
 //		this.Table.setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS);
+		
+		JComboBox<Integer> yearbox = new JComboBox<Integer>();
+		DriverService driverService = new DriverService(this.connection);
+		ArrayList<Integer> yearsArrayList = driverService.getStatsYear();
+		for(int s : yearsArrayList)
+			yearbox.addItem(s);
+		yearbox.setBounds(561, 245, 79, 22);
+		frame.getContentPane().add(yearbox);
+		
 		this.Table.getSelectionModel().addListSelectionListener(new ListSelectionListener() {
 
 			@Override
@@ -82,7 +96,7 @@ public class NavigationWindow {
 			
 		};
 		this.Team.addActionListener(TeamListener);
-		this.Team.setBounds(550, 244, 90, 25);
+		this.Team.setBounds(243, 244, 90, 25);
 		
 		this.WorksFor = new JButton("WorksFor");
 		ActionListener WorksForListener = new ActionListener() {
@@ -100,7 +114,7 @@ public class NavigationWindow {
 			
 		};
 		this.WorksFor.addActionListener(WorksForListener);
-		this.WorksFor.setBounds(143, 385, 90, 25);
+		this.WorksFor.setBounds(343, 244, 90, 25);
 		
 		this.Race = new JButton("Race");
 		ActionListener RaceListener = new ActionListener() {
@@ -118,7 +132,7 @@ public class NavigationWindow {
 			
 		};
 		this.Race.addActionListener(RaceListener);
-		this.Race.setBounds(298, 244, 90, 25);
+		this.Race.setBounds(143, 244, 90, 25);
 
 		this.Driver = new JButton("Driver");
 		ActionListener DriverListener = new ActionListener() {
@@ -145,8 +159,8 @@ public class NavigationWindow {
 			public void actionPerformed(ActionEvent e) {
 				try {
 					model = new DefaultTableModel();
-					Table.setModel(model);
-					queryData("Stats");
+					Table.setModel(model); 
+					queryStats((int) yearbox.getSelectedItem());
 				} catch (Exception e1) {
 					e1.printStackTrace();
 				}				
@@ -154,10 +168,10 @@ public class NavigationWindow {
 			
 		};
 		this.Stats.addActionListener(StatsListener);
-		this.Stats.setBounds(43, 316, 90, 25);
+		this.Stats.setBounds(443, 244, 90, 25);
 		
 		JButton update = new JButton("Update");
-		update.setBounds(43, 385, 90, 25);
+		update.setBounds(143, 474, 90, 25);
 		ActionListener updateListener = new ActionListener() {
 
 		
@@ -169,7 +183,7 @@ public class NavigationWindow {
 			
 		};
 		JButton personal = new JButton("Personal");
-		personal.setBounds(550, 385, 90, 25);
+		personal.setBounds(432, 474, 90, 25);
 		ActionListener personalListener = new ActionListener() {
 
 		
@@ -189,6 +203,7 @@ public class NavigationWindow {
 		this.frame.getContentPane().add(personal);
 		this.frame.getContentPane().add(Stats);
 		this.frame.getContentPane().add(WorksFor);
+		
 		this.frame.setVisible(true);
 	}
 	
@@ -204,8 +219,6 @@ public class NavigationWindow {
 			query = "{? = call get_All_Drivers}";
 		else if(tableName.equals("Race")) 
 			query = "{? = call get_All_Races}";
-		else if (tableName.equals("Stats"))
-			query = "{? = call get_all_stats}";
 		else if(tableName.equals("WorksFor"))
 			query = "{? = call get_all_WorksFor}";
 		CallableStatement cs = this.connection.getConnection().prepareCall(query);
@@ -261,5 +274,68 @@ public class NavigationWindow {
     		column.setPreferredWidth(width);
     	}
     	
+	}
+	
+	public void queryStats(int year) {
+		String query = "? = call get_Stats(?)";
+		
+		try {
+			CallableStatement cs;
+			cs = this.connection.getConnection().prepareCall(query);
+			cs.registerOutParameter(1, Types.INTEGER);
+			cs.setInt(2, year);
+			ResultSet rs = cs.executeQuery();
+			ResultSetMetaData rsmd = rs.getMetaData();
+			int count = rsmd.getColumnCount();
+			for(int i = 1; i <= count; i++) {
+				this.model.addColumn(rsmd.getColumnName(i));
+			}
+	    	String[] rowData = new String[count];
+	    	while(rs.next()) {
+	    		for(int i = 0; i < count; i++) 
+	    			rowData[i] = rs.getString(i + 1);
+	    		this.model.addRow(rowData);
+	    	}
+	    	this.Table.setAutoResizeMode( JTable.AUTO_RESIZE_OFF );
+	    	int totalWidth = 0; 
+	    	for (int c = 0; c < this.Table.getColumnCount(); c++)
+	    	{
+	    	    TableColumn column = this.Table.getColumnModel().getColumn(c);
+	    	    int preferredWidth = column.getMinWidth();
+	    	    int maxWidth = column.getMaxWidth();
+	    	    for (int r = 0; r < this.Table.getRowCount(); r++)
+	    	    {
+	    	        TableCellRenderer rend = this.Table.getCellRenderer(r, c);
+	    	        Component comp = this.Table.prepareRenderer(rend, r, c);
+	    	        int width = comp.getPreferredSize().width + this.Table.getIntercellSpacing().width;
+	    	        preferredWidth = Math.max(preferredWidth, width);
+	    	        if (preferredWidth >= maxWidth)
+	    	        {
+	    	            preferredWidth = maxWidth;
+	    	            break;
+	    	        }
+	    	    }
+	    	    column.setPreferredWidth( preferredWidth );
+	    	    totalWidth += preferredWidth;
+	    	}
+	    	if (totalWidth < this.Table.getWidth()) {
+	    		int totalOffset = this.Table.getWidth() - totalWidth;
+	    		int offset = totalOffset / this.Table.getColumnCount();
+	    		for (int c = 0; c < this.Table.getColumnCount(); c++) {
+	    			TableColumn column = this.Table.getColumnModel().getColumn(c);
+	    			int width = column.getPreferredWidth();
+	    			width += offset;
+	    			totalOffset -= offset;
+	    			column.setPreferredWidth(width);
+	    		}
+	    		TableColumn column = this.Table.getColumnModel().getColumn(0);
+	    		int width = column.getPreferredWidth();
+	    		width += totalOffset;
+	    		column.setPreferredWidth(width);
+	    	}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
 	}
 }
